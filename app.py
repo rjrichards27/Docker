ea3e65e56536977378156a9fd8fed2e3f5c1e8d7
rjrichards27 <89543602+rjrichards27@users.noreply.github.com>
2021-10-27 10:12:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import click
 @click.command()
 @click.option("--name")
 def hello(name):
-    click.echo(f'Hello {name}!')
+    click.echo(f'How are you, {name}?')
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
